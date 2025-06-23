909ba73e2919452cf5a5104dfa6a87677b62f0c2
MohammedNasser <U19200083@sharjah.ac.ae>
2025-06-23 14:36:44 +0400

    Fixed the updating of passing tests in the form

--- DLVD Project/ApplicationUC/UC_TakeWrittenTest.cs
+++ DLVD Project/ApplicationUC/UC_TakeWrittenTest.cs
@@ -66,6 +66,8 @@ namespace DLVD_Project.ApplicationUC
                 Test.IsLocked = true;
                 Test.Save(); // to save it is locked and finished 
                 MessageBox.Show("Saved Succussfully");
+                View.PassedTestCount++;
+
             }
             else
                 MessageBox.Show("Failed to be saved");
